cf1412645ac0f6891f4030ffbc0a47a577f74be8
souravbsk <souravbsk01@gmail.com>
2023-06-09 16:30:32 +0600

    review section added

--- src/DashBoard/Admins/ReviewClasses/ReviewClasses.jsx
+++ src/DashBoard/Admins/ReviewClasses/ReviewClasses.jsx
@@ -1,11 +1,119 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { Rating } from '@smastrom/react-rating'
 
+import '@smastrom/react-rating/style.css'
+import Swal from "sweetalert2";
 const ReviewClasses = () => {
-    return (
-        <div>
-            
+    const { id } = useParams();
+    const [rating, setRating] = useState(0)
+  const [axiosSecure] = useAxiosSecure();
+  const { data: manageClassReview, isLoading: isManageClassReview } = useQuery({
+    queryKey: ["reviewClass", id],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/manageClasses/${id}`);
+      return res.data;
+    },
+  });
+  console.log(manageClassReview);
+  console.log(rating);
+
+
+  const handleReviewSubmit = (e) => {
+    e.preventDefault()
+    const reviewText = e.target.reviewDetail.value;
+    const review = {
+        rating,
+        reviewText,
+    }
+
+    axiosSecure
+      .put(`/manageClasses/${id}`, review)
+      .then((res) => {
+        if (res?.data?.modifiedCount > 0) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `Class Review Update Done`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+
+
+   
+  }
+
+
+
+
+
+
+
+
+
+
+  
+
+
+  return (
+    <div className="w-full p-3 md:p-20">
+      <div>
+        <div className="card border flex-col md:flex-row overflow-hidden card-side bg-base-100 shadow-xl">
+          <img
+            className="md:w-96 h-72 max-w-full"
+            src={manageClassReview?.classImage}
+            alt="Movie"
+          />
+
+          <div className="card-body">
+            <h2 className="card-title">{manageClassReview?.className}</h2>
+            <p>availableSeats: {manageClassReview?.availableSeats}</p>
+            <p>enrolled: {manageClassReview?.enrolled}</p>
+            <p>instructorEmail: {manageClassReview?.instructorEmail}</p>
+            <p>instructorName: {manageClassReview?.instructorName}</p>
+            <p className="font-bold text-2xl">
+              price: ${manageClassReview?.price}
+            </p>
+            <div className="card-actions justify-end">
+              {
+                <span className="badge badge-primary">
+                  {manageClassReview?.status}
+                </span>
+              }
+            </div>
+          </div>
         </div>
-    );
+
+        <div className="bg-slate-100 border  rounded-2xl shadow-lg mt-20">
+            <h2 className="text-center md:text-2xl font-medium my-6">Rate This Course</h2>
+            <div className="flex items-center justify-center">
+            <Rating style={{ maxWidth: 200 }} value={rating} onChange={setRating} />
+            </div>
+          <form onSubmit={handleReviewSubmit} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Kindly express your review in short way.</span>
+              </label>
+              <textarea
+                type="text"
+                required
+                name="reviewDetail"
+                placeholder="Review in detail"
+                className="input border p-5 h-40 resize-none"
+              ></textarea>
+            </div>
+            <div className="form-control mt-6">
+              <button className="">Review Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewClasses;
--- src/DashBoard/Instructor/InstructorClassesList/InstructorClassesList.jsx
+++ src/DashBoard/Instructor/InstructorClassesList/InstructorClassesList.jsx
@@ -5,51 +5,52 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
 import { FaTrashAlt } from "react-icons/fa";
 import Swal from "sweetalert2";
+import { Rating } from "@smastrom/react-rating";
 
 const InstructorClassesList = () => {
   const [axiosSecure] = useAxiosSecure();
   const { user } = useAuth();
   const {
-    data: classesData,
+    data: classesData = [],
     isLoading: isLoadingClasses,
     refetch,
   } = useQuery({
-    queryKey: ["instructorClassList",user?.email],
+    queryKey: ["instructorClassList", user?.email],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/instructorClasses?email=${user?.email}`);
+      const res = await axiosSecure.get(
+        `/instructorClasses?email=${user?.email}`
+      );
       return res.data;
     },
   });
 
-
-
   console.log(classesData);
   const handleRemoveClass = (id) => {
     Swal.fire({
-        title: "Are you sure?",
-        text: "You won't to remove this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          axiosSecure.delete(`/manageClasses/${id}`).then((res) => {
-            if (res?.data?.deletedCount > 0) {
-              refetch();
-              Swal.fire({
-                position: "center",
-                icon: "success",
-                title: "Class Deleted Done",
-                showConfirmButton: false,
-                timer: 1500,
-              });
-            }
-          });
-        }
-      });
-  }
+      title: "Are you sure?",
+      text: "You won't to remove this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/manageClasses/${id}`).then((res) => {
+          if (res?.data?.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "Class Deleted Done",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div className="w-full p-3 md:p-12">
@@ -62,14 +63,18 @@ const InstructorClassesList = () => {
               <tr>
                 <th>#</th>
                 <th>Course</th>
-                <th>Instructor</th>
+                <th>enroll</th>
+                <th>availableSeats</th>
+                <th>price</th>
+  
+                <th>Review</th>
                 <th>Status</th>
                 <th>Action</th>
               </tr>
             </thead>
             <tbody>
               {classesData?.map((classItem, i) => (
-                <tr key={classItem._id}>
+                <tr className={classItem?.status === "denied" && "bg-red-300"} key={classItem._id}>
                   <th>{i + 1}</th>
                   <td>
                     <div className="flex items-center space-x-3">
@@ -89,22 +94,49 @@ const InstructorClassesList = () => {
                       </div>
                     </div>
                   </td>
+                  <td className="font-bold">{classItem?.enrolled}</td>
+                  <td className="font-bold">{classItem?.availableSeats}</td>
+                  <td className="font-bold">${classItem?.price}</td>
+                  
                   <td>
-                    <p> Email: {classItem?.instructorEmail}</p>
+                    <div>
+                      <Rating
+                      readOnly
+                        style={{ maxWidth: 100 }}
+                        value={classItem?.review?.rating || 0}
+                       
+                      />
+                    </div>
                     <br />
-                    <span className="badge badge-ghost badge-md">
-                      Name: {classItem?.instructorName}
+                    <span >
+                      {classItem?.review?.reviewText || "  ..."}
                     </span>
                   </td>
                   <th>
-                    {classItem?.status === "pending" ?  <button disabled className="badge text-black bg-warning badge-primary">pending</button> 
-                :
-                classItem?.status === "approved" ? <button disabled className="badge text-black bg-green-400 badge-primary">approved</button>    
-                :
-                <button disabled className="badge bg-red-400 text-black badge-primary">denied</button>
-                }
+                    {classItem?.status === "pending" ? (
+                      <button
+                        disabled
+                        className="badge text-black bg-warning badge-primary"
+                      >
+                        pending
+                      </button>
+                    ) : classItem?.status === "approved" ? (
+                      <button
+                        disabled
+                        className="badge text-black bg-green-400 badge-primary"
+                      >
+                        approved
+                      </button>
+                    ) : (
+                      <button
+                        disabled
+                        className="badge bg-red-400 text-black badge-primary"
+                      >
+                        denied
+                      </button>
+                    )}
                   </th>
-                  
+
                   <th>
                     <button
                       onClick={() => handleRemoveClass(classItem?._id)}
